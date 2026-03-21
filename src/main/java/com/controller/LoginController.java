66dcdbc758bd34fae9cb317deb517b2f08f9358e
rohan <rohansherestha612@gmail.com>
2026-03-21 16:05:03 +0545

    Design Fixes

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/LoginController")
+@WebServlet("/login")
 public class LoginController extends HttpServlet {
 
     @Override
