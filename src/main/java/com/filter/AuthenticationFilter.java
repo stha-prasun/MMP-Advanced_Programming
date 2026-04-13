f6c6165641478bf80231ad3f089939778cde4733
Sanrose <sanrosebhetuwal@gmail.com>
2026-04-13 12:53:20 +0545

    Created a controller for homepage

--- src/main/java/com/filter/AuthenticationFilter.java
+++ src/main/java/com/filter/AuthenticationFilter.java
@@ -1,36 +1,36 @@
-package com.filter;
-
-import com.util.SessionUtil;
-import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-@WebFilter(asyncSupported = true, urlPatterns = "/*")
-public class AuthenticationFilter extends HttpFilter implements Filter {
-    private static final String LOGIN = "/login";
-    private static final String REGISTER = "/register";
-    private static final String HOME = "/home";
-    private static final String ROOT = "/";
-
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException
-    {
-        // Cast the request and response to HttpServletRequest and HttpServletResponse
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-        // Get the requested URI
-        String uri = req.getRequestURI();
-        //boolean isLoggedIn = SessionUtil.getAttribute(req, "Email") != null;
-        if (uri.endsWith(".css") || uri.endsWith(HOME) || uri.endsWith(ROOT)) {
-            chain.doFilter(request, response);
-            return;
-        }
-    }
-
-
-}
+//package com.filter;
+//
+//import com.util.SessionUtil;
+//import jakarta.servlet.*;
+//import jakarta.servlet.annotation.WebFilter;
+//import jakarta.servlet.http.HttpFilter;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//
+//@WebFilter(asyncSupported = true, urlPatterns = "/*")
+//public class AuthenticationFilter extends HttpFilter implements Filter {
+//    private static final String LOGIN = "/login";
+//    private static final String REGISTER = "/register";
+//    private static final String HOME = "/home";
+//    private static final String ROOT = "/";
+//
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException
+//    {
+//        // Cast the request and response to HttpServletRequest and HttpServletResponse
+//        HttpServletRequest req = (HttpServletRequest) request;
+//        HttpServletResponse res = (HttpServletResponse) response;
+//        // Get the requested URI
+//        String uri = req.getRequestURI();
+//        //boolean isLoggedIn = SessionUtil.getAttribute(req, "Email") != null;
+//        if (uri.endsWith(".css") || uri.endsWith(HOME) || uri.endsWith(ROOT)) {
+//            chain.doFilter(request, response);
+//            return;
+//        }
+//    }
+//
+//
+//}
