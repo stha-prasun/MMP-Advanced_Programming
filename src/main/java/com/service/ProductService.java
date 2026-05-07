eb3fffe0e54f4311fb55824e328c5b0c72c33bcb
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-05-07 14:32:39 +0545

    Get Product By Id

--- src/main/java/com/service/ProductService.java
+++ src/main/java/com/service/ProductService.java
@@ -2,15 +2,12 @@ package com.service;
 
 import com.dao.ProductDAO;
 import com.model.Product;
-import com.util.DBconfig;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class ProductService {
-    public void addSeller(String productName, int price, String category, LocalDateTime postedAt, String description, String imgUrl) throws Exception {
+    public void addProduct(String productName, int price, String category, LocalDateTime postedAt, String description, String imgUrl) throws Exception {
 
 
         ProductDAO dao = new ProductDAO();
@@ -26,9 +23,11 @@ public class ProductService {
 
     }
 
-    public Product getProductById() throws Exception {
+    public Product getProductById(Long id) throws Exception {
         ProductDAO dao = new ProductDAO();
 
+        Product product = getProductById(id);
 
+        return product;
     }
 }
